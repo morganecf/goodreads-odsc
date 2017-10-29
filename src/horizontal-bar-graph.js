c41e0962b047e1f346c163dc9201e200db255552
Morg <morgane@datarobot.com>
2017-10-29 19:50:05 -0400

    Horizontal bar graph

---
+++ src/horizontal-bar-graph.js
@@ -0,0 +1,67 @@
+// This function expects an array of data of the form { name, count }.
+function horizontalBarGraph(data, el, options={width: 500, height: 1000}) {
+  // Set a sizable margin so that the axis ticks don't get cut off
+  const margin = {
+    left: 120,
+    top: 20,
+  };
+
+  const yscaleRange = [margin.top, options.height];
+  const xscaleRange = [margin.left, options.width];
+
+  // The range (min and max) of the data. For example, if we have count values between
+  // 0 and 100, this will give us [0, 100].
+  const xrange = d3.extent(data, d => d.count);
+
+  const categories = data.map(d => d.name);
+
+  // Create a categorical scale. This will allow us to map from discrete categories,
+  // such as the name of a tag, to a pixel value in the drawing area. We'll use this
+  // to place the category names along the y-axis.
+  const yscale = d3.scaleBand().domain(categories).range(yscaleRange);
+
+  // Create a linear scale. This will allow us to map from the categories' numerical
+  // count vaues to a pixel value in the drawing area. We'll use this to place the
+  // x-axis ticks and size the width of the bars.
+  const xscale = d3.scaleLinear().domain(xrange).range(xscaleRange);
+
+  // These functions help us actually draw the axes. We'll place the x-axis at the top
+  // of the chart and y-axis to the left.
+  const xaxis = d3.axisTop().scale(xscale);
+  const yaxis = d3.axisLeft().scale(yscale);
+
+  // Start drawing elements! SVG elements like rects and circles can only be drawn within
+  // an svg container
+  const svg = el.append('svg')
+    .attr('width', options.width + margin.left)
+    .attr('height', options.height + margin.top);
+
+  // Render the axes within "g" or group elements. The axis-rendering function takes
+  // care of drawing all the lines and ticks and text elements comprising an axis.
+  svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(yaxis);
+  svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', `translate(0, ${margin.top})`)
+    .call(xaxis);
+
+  // scaleBand exposes a useful function that gives us the width of each band, or tick,
+  // on a categorical scale. Use this to determine the height of each horizontal bar.
+  // Subtract 2 to add a little space between the bars.
+  const barHeight = yscale.bandwidth() - 2;
+
+  // Draw the bars. The (x, y) position of each bar is the top-left corner of the rectangle.
+  // The width of the bar is scaled to the count associated with that category. The y-position
+  // is determined by the yscale, which will output a pixel value given a category name.
+  svg.selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', margin.left + 1)           // Add 1 to give some space between axis and bar
+    .attr('y', d => yscale(d.name) + 1)   // Add 1 to give some space between bars
+    .attr('width', d => xscale(d.count))
+    .attr('height', barHeight)
+    .attr('fill', '#3A539B');             // Bar color
+}
---
+++ src/network.js
@@ -0,0 +1,108 @@
+const forceWidth = 1250;
+const forceHeight = 450;
+const center = {x: forceWidth / 2, y: forceHeight / 2};
+
+function network(data) {
+  // Sets the node id accessor so that the force simulation
+  // knows that a node is uniquely identified by its name
+  const forceLink = d3.forceLink().id(d => d.name);
+
+  // Force function that acts upon multiple bodies. A positive
+  // strength causes nodes to attract each other -- this will
+  // cause nodes to collapse into each other. A negative strength
+  // causes nodes to repel each other.
+  const forceCharge = d3.forceManyBody().strength(-3);
+
+  // Creates a centering force, i.e., centers the simulation at
+  // the given x and y coordinates.
+  const forceCenter = d3.forceCenter(center.x, center.y);
+
+  // Create a new simulation with the above forces
+  const simulation = d3.forceSimulation()
+    .force('link', forceLink)
+    .force('charge', forceCharge)
+    .force('center', forceCenter);
+
+  // Create the svg element
+  const svg = createSvgContainer('#network', forceWidth, forceHeight, 0, 0);
+
+  // Create a color scale for the movie ratings. The higher
+  // the rating, the redder the movie node.
+  const rating = d3.scaleLinear()
+    .domain([3, 9])
+    .range([red, blue]);
+
+  // Add links or edges as path elements. Group all of these
+  // paths in a g element. Link width (thickness) represents
+  // the edge weights. The higher the weight, i.e., the # of
+  // keywords a pair of movies has in common, the bigger the
+  // width. Each edge or link represents a weighted connection
+  // between two movies, based on how many keywords they have
+  // in common.
+  const link = svg.append('g')
+    .attr('class', 'links')
+    .selectAll('.link')
+    .data(data.links)
+    .enter()
+    .append('line')
+    .attr('class', 'link')
+    .attr('stroke-width', d => d.overlap.length);
+
+  // Add node elements, grouped together. Each node represents
+  // a movie and is color-scaled based on its IMDB rating.
+  const node = svg.append('g')
+    .attr('class', 'nodes')
+    .selectAll('.node')
+    .data(data.nodes)
+    .enter()
+    .append('circle')
+    .attr('class', 'node')
+    .attr('r', 5)
+    .attr('fill', d => rating(d.score));
+
+  // Start the simulation
+  simulation.nodes(data.nodes).on('tick', tick);
+  simulation.force('link').links(data.links);
+
+  // This function is called on every "tick", or unit of time,
+  // of the simulation. In each tick, D3 generates new node
+  // positions based on the starting positions and the forces
+  // chosen above. These are stored as x and y attributes of each
+  // node data object. We reposition the svg links and nodes with
+  // these values. You can use this function to customize the
+  // positions of the nodes and links. For example, if you wanted
+  // to ensure some kind of hierarchical tree structure, you could
+  // give each node a level and then make sure nodes of a certain
+  // level stayed above larger levels and below smaller levels.
+  function tick() {
+    link
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y);
+
+    node
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y);
+  }
+
+  // Formats HTML for the node tooltips: show the movie
+  // name and its IMDB rating.
+  function nodeTooltipContent(d) {
+    const movie = d.name;
+    const rating = d.score;
+    return contentToHtmlFormatter({movie, rating});
+  }
+
+  // Formats HTML for the link tooltips: show the two
+  // movie names and the key words they have in common.
+  function linkTooltipContent(d) {
+    const link = d.source.name + ', ' + d.target.name;
+    const keywords = d.overlap.join(', ');
+    return contentToHtmlFormatter({link, keywords});
+  }
+
+  // Initialize the tooltips
+  addToolTip(svg, node, nodeTooltipContent);
+  addToolTip(svg, link, linkTooltipContent);
+}
