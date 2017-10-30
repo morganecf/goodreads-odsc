222118bd78d49c81fd989aab0dfdcab5a1245c13
Morg <morgane@datarobot.com>
2017-10-29 20:51:32 -0400

    Ratings over time

---
+++ src/ratings-over-time.js
@@ -0,0 +1,55 @@
+function ratingsOverTime(data, el, numBins=100, options={width: 600, height: 300}) {
+  const margin = {
+    left: 50,
+    bottom: 25,
+  };
+
+  // Create a histogram from the data
+  const hist = d3.histogram()
+    .value(d => d.pub_year)
+    .thresholds(numBins)
+    (data);
+
+  // This function computes the average rating in a given bin
+  const averageRating = bin => d3.mean(bin, d => d.avg_rating);
+
+  const binSize = options.width / hist.length;
+
+  // Ratings are on a scale of 0 to 5, though most ratings in this sample
+  // are pretty close together.
+  const yrange = d3.extent(hist, d => averageRating(d));
+  const xrange = d3.extent(data, d => d.pub_year);
+
+  const xscale = d3.scaleLinear().domain(xrange).range([margin.left, options.width]);
+  const yscale = d3.scaleLinear().domain(yrange).range([options.height, 0]);
+
+  const xaxis = d3.axisBottom().scale(xscale).tickFormat(d => new Date(d).getFullYear());
+  const yaxis = d3.axisLeft().scale(yscale);
+
+  // This function will generate pixel values for a line of the ratings over time
+  const line = d3.line()
+    .x(d => xscale((d.x0 + d.x1) / 2))
+    .y(d => yscale(averageRating(d)));
+
+  // Draw the svg container and axes
+  const svg = el.append('svg')
+    .attr('width', options.width + margin.left)
+    .attr('height', options.height + margin.bottom);
+
+  svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', `translate(0, ${options.height})`)
+    .call(xaxis);
+  svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(yaxis);
+
+  // Draw the line in a group element
+  svg.append('path')
+    .attr('class', 'line')
+    .datum(hist)
+    .attr('d', line)
+    .attr('fill', 'none')
+    .attr('stroke', 'red');
+}
