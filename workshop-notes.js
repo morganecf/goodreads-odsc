423ea1b96d345d52564fb52b0d7a7cad8a8d9cac
Nick Watts <nicholas.watts@datarobot.com>
2017-11-02 13:04:13 -0400

    workshop notes

--- workshop-notes.js
+++ workshop-notes.js
@@ -18,3 +18,29 @@ circle.transition().duration(2000).delay(1000).attr('opacity', 1).attr('r', 500)
 
 // remove the circle from the DOM
 circle.remove()
+
+
+// Create some toy data
+data = [{color: 'yellow', r: 5}, {color: 'blue', r: 10}, {color: 'purple', r: 15}];
+
+// Bind data to empty circle selection
+circle = svg.selectAll('circle').data(data, d => d.color);
+
+// There shouldbe nothing in our exit selection (old elements)
+circle.exit().data()
+[]
+
+// There should be all three elements in our enter selection (new elements)
+circle.enter().data()
+
+// Equivalent to d => d.r in the anonymous function below
+function getRadius(d) { return d.r; }
+getRadius(data[0])
+
+// Bind circles to the dom using enter()
+circle.enter()
+  .append("circle")
+  .attr('r', d => d.r)
+  .attr('fill', d => d.color)
+  .attr('cy', 50)
+  .attr('cx', (d, i) => 50 + i * 50)
