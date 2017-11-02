64f4eb49508a484b966859b7a8428ac33905cc4f
Morg <morgane@datarobot.com>
2017-11-02 08:55:10 -0700

    toy data

--- toy_data.js
+++ toy_data.js
@@ -1,14 +1,14 @@
 // bar chart data
 data = [
-  ['the republic', 5093],
-  ['the prince', 3253],
-  ['the stranger', 3121],
-  ['zen', 2932],
-  ['the art of war', 2348],
-  ['a history of western philosophy', 2270],
-  ['siddhartha', 2248],
-  ['being and nothingness', 1722],
-  ['being and time', 1704],
+  ['the republic', 5093, 3.5],
+  ['the prince', 3253, 3],
+  ['the stranger', 3121, 3.3],
+  ['zen', 2932, 2],
+  ['the art of war', 2348, 3],
+  ['a history of western philosophy', 2270, 4.1],
+  ['siddhartha', 2248, 3.2],
+  ['being and nothingness', 1722, 5],
+  ['being and time', 1704, 4.7],
 ];
 
 // Line data (weather from https://darksky.net/forecast/37.7793,-122.4192/us12/en)
